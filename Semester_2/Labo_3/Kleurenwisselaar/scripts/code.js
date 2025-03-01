af7580a055982e56e8945a00d123897f6fff7b8d
AidenStorme <aiden.storme@student.vives.be>
2025-03-01 11:49:07 +0100

    Kleurenwisselaar

--- Semester_2/Labo_3/Kleurenwisselaar/scripts/code.js
+++ Semester_2/Labo_3/Kleurenwisselaar/scripts/code.js
@@ -1,22 +1,20 @@
 const setup = () => {
 
-    document.getElementById("btnButton2").addEventListener("click", changeColor1);
+
+    const button1 = document.getElementById("btnButton1");
+    const button2 = document.getElementById("btnButton2");
+    const button3 = document.getElementById("btnButton3");
+
+    button1.addEventListener("click", () => toggleColor(button1));
+    button2.addEventListener("click", () => toggleColor(button2));
+    button3.addEventListener("click", () => toggleColor(button3));
+
 
 }
 
-const changeColor1 = () => {
+const toggleColor = (button) => {
 
-    const button2 = document.getElementById("btnButton2");
-    let i = 0;
-
-    if(i === 0){
-        button2.style.backgroundColor = "Black";
-        i = 1;
-    } else{
-        button2.style.backgroundColor = "Blue";
-        i = 0;
-    }
-
+    button.classList.toggle("active");
 }
 
 window.addEventListener("load", setup);
